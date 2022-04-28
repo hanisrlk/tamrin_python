87b9b74096ea8daeda589d82203abe4bd942cb1c
hanisrlk <hanisrlk78@gmail.com>
2022-04-28 15:05:30 +0430

    first project

---
+++ for i in range (5).py
@@ -0,0 +1,52 @@
+# for i in range (5): 
+#     print(i,end=' ')
+# print()
+
+# for i in range(0,10,2):
+#     print(i,end=',')
+
+    
+# for i in range (10):
+#     for j in range (0,i):
+#         print(() , end='')
+#     print()
+# for i in range(10,0,-1):
+#     for j in range (90,i):
+#         print (j , end=' ')
+# print()
+
+# height=5
+# stars=1
+# for i in range (height):
+#     print(' '*(height-i) + ('*'*stars))
+
+#     stars+=2
+
+# for i in range (1,11):
+#     if i ==5:
+#         break
+#     print(i)
+
+# import string
+# print('alphabet from a-z:')
+# for letter in string.ascii_lowercase:
+#     if letter=="i":
+#         break
+#     print(letter , end=' ')
+
+
+
+
+# name= 'haniye'
+# print(name[0])
+num=100
+if num > 1:
+    # check for factors
+    for i in range(2, num):
+        if (num % i) == 0:
+            break
+    print(num)
+
+
+
+
